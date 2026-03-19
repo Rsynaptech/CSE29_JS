61d169c121875d662efa54b9f2ff94c101fbee68
Rsynaptech <raghvendrayadav3399@gmail.com>
2026-03-19 13:44:42 +0530

    added

---
+++ funcal.js
@@ -0,0 +1,17 @@
+function calculation(){
+    let n=document.getElementsByid("subject").value;
+    let i;
+    let total;
+    for(i=0;i<n;i++){
+        let x=parseFloat(prompt("enter the marks of subject" + (1)));
+        total+=n;
+        let average = total/n;
+        let grade;
+        if(average>90)
+            grade ='A+';
+        else{
+
+        }
+        let result = document.getelementbyid.("result").inner
+    }
+}
